57bb6758ee6246cd2152a63f840d7ee52fd9b43a
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-14 08:28:48 +0500

    1

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nur_note_app/data/models/notes_model.dart';
+import 'package:nur_note_app/utils/styles/app_text_style.dart';
 import 'package:nur_note_app/utils/utility_functions/utility_functions.dart';
 
 class DetailScreen extends StatelessWidget {
@@ -13,8 +14,15 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion(
-      child: Scaffold(),
       value: systemUiOverlayStyle,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Detail',
+            style: AppTextStyle.nunitoBold,
+          ),
+        ),
+      ),
     );
   }
 }
